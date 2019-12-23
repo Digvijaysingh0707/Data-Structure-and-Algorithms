86c69fcc08e8839d12d7a06c8d52a2b24f6679dc
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2019-12-23 11:42:12 +0530

    Main.java

---
+++ Linked List/CRX-Add Last/Main.java
@@ -0,0 +1,30 @@
+
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+        list.addLast(10);
+        list.addLast(14);
+        list.addLast(15);
+        list.addLast(19);
+        list.addLast(17);
+        list.display();
+
+
+
+
+
+
+    }
+
+
+
+}
