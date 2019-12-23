f24f84fc04a9b9392460b0c990d8b7a6d9040705
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2019-12-23 11:55:58 +0530

    Main.java

---
+++ Linked List/CRX-Add First/Main.java
@@ -0,0 +1,30 @@
+
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+        list.addFirst(10);
+        list.addLast(14);
+        list.addFirst(15);
+        list.addLast(19);
+        list.addLast(17);
+        list.display();
+
+
+
+
+
+
+    }
+
+
+
+}
