3b4626e6f901cea2652d2e115cd7221e066f72c0
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2019-12-24 22:52:14 +0530

    Main.java

---
+++ Linked List/Find length/Main.java
@@ -0,0 +1,31 @@
+
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Start with the empty list */
+        LinkedList llist = new LinkedList();
+
+        llist.push(1);
+        llist.push(4);
+        llist.push(13);
+        llist.push(12);
+        llist.push(12);
+        llist.push(11);
+        System.out.print("Number of nodes: " + llist.getCount());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
