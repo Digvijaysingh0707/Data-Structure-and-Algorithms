a94f8e9c145fd08963cca2e9438013dbd8b2b053
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-01-25 00:36:24 +0530

    Create revese stack using recursion.java

---
+++ Stack/revese stack using recursion.java
@@ -0,0 +1,51 @@
+import java.util.List;
+import java.util.Stack;
+
+
+public class LinkedList {
+
+    public static Stack<Integer> revese(Stack<Integer> st){
+        if(st.isEmpty()) return st;
+        int temp=st.pop();
+        revese(st);
+        insertAtBottom(st,temp);
+        return st;
+}
+public static void insertAtBottom(Stack<Integer> st,int x){
+        if(st.isEmpty()){
+            st.push(x);
+            return;
+        }
+        int temp=st.pop();
+        insertAtBottom(st,x);
+        st.push(temp);
+}
+
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        revese(s);
+        while(s.isEmpty()==false){
+            System.out.println(s);
+            break;
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
