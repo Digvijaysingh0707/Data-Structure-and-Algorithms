8a4c2b0e6983a39045645422360156981225e583
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-01-31 19:34:30 +0530

    Create  Binary Tree - Diameter of Tree.java

---
+++ Binary Tree/Binary Tree - Diameter of Tree.java
@@ -0,0 +1,73 @@
+It have a complexity of O(n^2)
+
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Node {
+    int key;
+    Node left,right;
+    public Node(int item){
+        this.key=item;
+        left=right=null;
+
+    }
+}
+class  BinaryTree {
+     Node root;
+    static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int Lheight=height(root.left);
+        int Rheight=height(root.right);
+
+        if(Lheight>Rheight){
+            return (Lheight+1);
+
+        }
+        else{
+            return (Rheight+1);
+        }
+
+
+
+    }
+     static int diameter(Node root){
+         if(root==null){
+             return 0;
+         }
+         int h1=height(root.left);
+         int h2=height(root.right);
+         int Op1=h1+h2;
+         int Op2=diameter(root.left);
+         int Op3=diameter(root.right);
+         return Math.max(Op1,Math.max(Op2,Op3));
+     }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root=new Node(1);
+
+        tree.root.left=new Node(2);
+
+        tree.root.right=new Node(3);
+
+        tree.root.left.left=new Node(4);
+
+        tree.root.left.right = new Node(5);
+        System.out.println(diameter(tree.root));
+
+
+
+
+
+    }
+}
