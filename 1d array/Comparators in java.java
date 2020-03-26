2121da53cfde26c31e172fa7f6b422c898194e91
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-03-26 14:21:14 +0530

    Create Comparators in java.java

---
+++ 1d array/Comparators in java.java
@@ -0,0 +1,101 @@
+//Geeks for geeks
+package com.company;
+import java.security.KeyPair;
+import java.util.*;
+class Student
+{
+    int rollno;
+    String name, address;
+
+    // Constructor
+    public Student(int rollno, String name,
+                   String address)
+    {
+        this.rollno = rollno;
+        this.name = name;
+        this.address = address;
+    }
+
+    // Used to print student details in main()
+    public String toString()
+    {
+        return this.rollno + " " + this.name +
+                " " + this.address;
+    }
+}
+
+class Sortbyroll implements Comparator<Student>
+{
+    // Used for sorting in ascending order of
+    // roll number
+    public int compare(Student a, Student b)
+    {
+        return a.rollno - b.rollno;
+    }
+}
+
+class Sortbyname implements Comparator<Student>
+{
+    // Used for sorting in ascending order of
+    // roll name
+    public int compare(Student a, Student b)
+    {
+        return a.name.compareTo(b.name);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> ar = new ArrayList<Student>();
+        ar.add(new Student(111, "bbbb", "london"));
+        ar.add(new Student(131, "aaaa", "nyc"));
+        ar.add(new Student(121, "cccc", "jaipur"));
+        ar.add(new Student(171,"dig","kanpur"));
+
+        System.out.println("Unsorted");
+        for (int i = 0; i < ar.size(); i++)
+            System.out.println(ar.get(i));
+
+        Collections.sort(ar, new Sortbyroll());
+
+        System.out.println("\nSorted by rollno");
+        for (int i = 0; i < ar.size(); i++)
+            System.out.println(ar.get(i));
+
+        Collections.sort(ar, new  Sortbyname());
+
+        System.out.println("\nSorted by name");
+        for (int i = 0; i < ar.size(); i++)
+            System.out.println(ar.get(i));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
