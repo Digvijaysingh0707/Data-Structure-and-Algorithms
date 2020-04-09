dd6a3f428d9d6dad2413e998ee5ef0f30547e452
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-04-09 22:19:17 +0530

    Create Main.java

---
+++ LinkedList1/Adding Node/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /* Start with the empty list */
+        LinkedList llist = new LinkedList();
+
+        // Insert 6.  So linked list becomes 6->NUllist
+        llist.append(6);
+
+        // Insert 7 at the beginning. So linked list becomes
+        // 7->6->NUllist
+        llist.push(7);
+
+        // Insert 1 at the beginning. So linked list becomes
+        // 1->7->6->NUllist
+        llist.push(1);
+
+        // Insert 4 at the end. So linked list becomes
+        // 1->7->6->4->NUllist
+        llist.append(4);
+
+        // Insert 8, after 7. So linked list becomes
+        // 1->7->8->6->4->NUllist
+        llist.insertAfter(llist.head.next, 8);
+
+        System.out.println("\nCreated Linked list is: ");
+        llist.printList();
+
+    }
+}
