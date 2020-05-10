ce5755dd651347683c30a48d2db50d797e2cda33
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-05-10 09:37:14 +0530

    Update and rename 6.height Balanced tree.java to 88.height Balanced tree.java

--- Binary Tree/88.height Balanced tree.java
+++ Binary Tree/88.height Balanced tree.java
@@ -5,6 +5,8 @@ Compelxity:- O(n2) tree in which abs(h1-h2)<=1
     O(n^2)
     
     
+    
+    
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -143,10 +145,85 @@ class  BinaryTree {
     }
 }
 
+        
+    Another solution using O(n)    
+        import java.util.concurrent.atomic.AtomicBoolean;
 
+// Data structure to store a Binary Tree node
+class Node
+{
+	int data;
+	Node left = null, right = null;
 
-
-
-
-    }
+	Node(int data) {
+		this.data = data;
+	}
 }
+
+class Main {
+	// Recursive function to check if given binary tree is height balanced or not
+	public static int isHeightBalanced(Node root, AtomicBoolean isBalanced)
+	{
+		// base case: tree is empty or tree is not balanced
+		if (root == null || !isBalanced.get()) {
+			return 0;
+		}
+
+		// get height of left subtree
+		int left_height = isHeightBalanced(root.left, isBalanced);
+
+		// get height of right subtree
+		int right_height = isHeightBalanced(root.right, isBalanced);
+
+		// tree is unbalanced if absolute difference between height of
+		// its left subtree and right subtree is more than 1
+		if (Math.abs(left_height - right_height) > 1) {
+			isBalanced.set(false);
+		}
+
+		// return height of subtree rooted at current node
+		return Math.max(left_height, right_height) + 1;
+	}
+
+	// Main function to check if given binary tree is height balanced or not
+	public static boolean isHeightBalanced(Node root)
+	{
+		// Using AtomicBoolean as boolean is passed by value in Java
+		AtomicBoolean isBalanced =  new AtomicBoolean(true);
+		isHeightBalanced(root, isBalanced);
+
+		return isBalanced.get();
+	}
+
+	// main function
+	public static void main(String[] args) {
+	    /* Construct below tree
+		          1
+		        /   \
+		       /     \
+		      2       3
+		     / \     /
+		    4   5   6
+	    */
+
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+
+		if (isHeightBalanced(root)) {
+			System.out.print("Yes");
+		} else {
+			System.out.print("No");
+		}
+	}
+}
+
+
+
+
+
+  
+
