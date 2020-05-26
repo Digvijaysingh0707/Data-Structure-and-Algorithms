f8600a3b17499d95fafde7f343751acaf9521c70
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-05-26 19:38:51 +0530

    Update revese stack using recursion.java

--- Stack/revese stack using recursion.java
+++ Stack/revese stack using recursion.java
@@ -1,26 +1,28 @@
-import java.util.List;
+package Stack;
+
 import java.util.Stack;
 
-
-public class LinkedList {
-
-    public static Stack<Integer> revese(Stack<Integer> st){
-        if(st.isEmpty()) return st;
-        int temp=st.pop();
-        revese(st);
-        insertAtBottom(st,temp);
-        return st;
-}
-public static void insertAtBottom(Stack<Integer> st,int x){
-        if(st.isEmpty()){
-            st.push(x);
+public class ReverseStackRecursion {
+    static void InsertAtBottom(Stack<Integer> s, int x) {
+        if (s.isEmpty()) {
+            s.push(x);
             return;
         }
-        int temp=st.pop();
-        insertAtBottom(st,x);
-        st.push(temp);
-}
+        int x1 = s.peek();
+        s.pop();
+        InsertAtBottom(s,x);
+        s.push(x1);
+    }
 
+
+    static void reverse(Stack<Integer> s){
+        if (s.isEmpty())return;
+        int x=s.peek();
+        s.pop();
+        reverse(s);
+        InsertAtBottom(s,x);
+
+    }
 
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
@@ -28,24 +30,11 @@ public static void insertAtBottom(Stack<Integer> st,int x){
         s.push(2);
         s.push(3);
         s.push(4);
-        revese(s);
+        reverse(s);
         while (s.isEmpty()==false){
-                System.out.print(s.peek()+" ");
-                s.pop();
-            }
+            System.out.println(s.peek()+" ");
+            s.pop();
 
-
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
