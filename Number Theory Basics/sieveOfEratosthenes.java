7b0312ff7fb6dd11688e2d09acfd078fb50ea00d
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-06-27 18:46:45 +0530

    Update sieveOfEratosthenes.java

--- Number Theory Basics/sieveOfEratosthenes.java
+++ Number Theory Basics/sieveOfEratosthenes.java
@@ -1,96 +1,49 @@
 Time complexity : O(n*log(log(n)))
 
 
-// from gfg
-package com.company;
-import java.security.KeyPair;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main {
-    static void sieveOfEratosthenes(int n) {
-        // Create a boolean array "prime[0..n]" and initialize
-        // all entries it as true. A value in prime[i] will
-        // finally be false if i is Not a prime, else true.
-        boolean prime[] = new boolean[n + 1];
-        for (int i = 0; i < n; i++)
-            prime[i] = true;
-        for (int p = 2; p * p <= n; p++) {
-            // If prime[p] is not changed, then it is a prime
-            if (prime[p] == true) {
-                // Update all multiples of p
-                for (int i = p * p; i <= n; i += p)
-                    prime[i] = false;
+public class PrimeSieve {
+    static void primeSieve(int p[]){
+        //first mark all odd number as prime
+        for (int i=3;i<1000000;i+=2){
+            p[i]=1;
+        }
+        //sieve
+        for (long i=3;i<1000000;i+=2){
+            //if the current number is not marked then it is prime
+            if (p[(int)i]==1){
+//mark all multiple of i as not prime
+                for (long j=i*i;j<1000000;j+=i){
+                    p[(int)j]=0;
+                }
             }
         }
-
-            for (int i = 2; i <= n; i++) {
-                if (prime[i] == true)
-                    System.out.print(i + " ");
-
-            }
-        }
-
-
-
+        //special case
+        p[2]=1;// 2 is a prime
+        p[1]=p[0]=0;//0 and 1 is not prime
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int p[]= new int[10000005];
+        Arrays.fill(p,0);
+        int count=0;
+        primeSieve(p);
+        //lets print prime upto range n
+        for (int i=0;i<=n;i++){
+            if (p[i]==1){
+                count++;
+                System.out.print(i+" ");
+            }
 
-        System.out.print("Following are the prime numbers ");
-        System.out.println("smaller than or equal to " + n);
-       Main g=new Main();
-             g.sieveOfEratosthenes(n);
+        }
+
+
+
+
+
 
     }
 }
-//Coding blocks
-
-public class Main {
-        static void primeSieve(int[] p){
-            //mark all odd number,even numbers will not be prime
-            for (int i=3;i<=100000;i+=2){
-                p[i]=1;
-            }
-            for (long i=3;i<=100000;i++){
-                if (p[(int)i]==1){
-                    // mark all ultiples of i as not prime
-                    for (long j=i*i;j<=1000000;j+=i){
-                        p[(int)j]=0;
-                    }
-                }
-            }
-            p[2]=1;
-            p[1]=0;
-        }
-
-
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            int p[]= new int[10000005];
-            Arrays.fill(p,0);
-            int count=0;
-            primeSieve(p);
-            for (int i=0;i<=n;i++){
-                if (p[i]==1){
-                    count++;
-                    System.out.print(i+" ");
-                }
-
-            }
-
-            System.out.println("Total = "+count);
-
-
-
-
-
-        }
-    }
-
-
-
-
-
-
-
